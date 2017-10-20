f8388627f73908ab4a75da8a502c44b569957bff
Sergey Koshkin <koshkin.sergey@gmail.com>
2017-10-20 20:00:44 +0300

    Optimized the hardware layer

---
+++ src/arch/cortex_m/hal_cm.c
@@ -0,0 +1,293 @@
+/*
+ * Copyright (C) 2017 Sergey Koshkin <koshkin.sergey@gmail.com>
+ * All rights reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the License); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Project: uKernel real-time kernel
+ */
+
+/*******************************************************************************
+ *  includes
+ ******************************************************************************/
+
+#include "tn.h"
+#include "tn_arch.h"
+#include "tn_tasks.h"
+
+/*******************************************************************************
+ *  external declarations
+ ******************************************************************************/
+
+extern void task_exit(void);
+
+/*******************************************************************************
+ *  defines and macros (scope: module-local)
+ ******************************************************************************/
+
+/* Interrupt Control State Register Address */
+#define ICSR_ADDR                     (0xE000ED04)
+
+/* pendSV bit in the Interrupt Control State Register */
+#define PENDSVSET                     (0x10000000)
+
+/* System Handlers 12-15 Priority Register Address */
+#define PR_12_15_ADDR                 (0xE000ED20)
+
+/* PRI_14 (PendSV) priority in the System Handlers 12-15 Priority Register Address
+PendSV priority is minimal (0xFF) */
+#define PENDS_VPRIORITY               (0x00FF0000)
+
+/*******************************************************************************
+ *  typedefs and structures (scope: module-local)
+ ******************************************************************************/
+
+/*******************************************************************************
+ *  global variable definitions  (scope: module-exported)
+ ******************************************************************************/
+
+/*******************************************************************************
+ *  global variable definitions (scope: module-local)
+ ******************************************************************************/
+
+/*******************************************************************************
+ *  function prototypes (scope: module-local)
+ ******************************************************************************/
+
+/*******************************************************************************
+ *  function implementations (scope: module-local)
+ ******************************************************************************/
+
+/*******************************************************************************
+ *  function implementations (scope: module-exported)
+ ******************************************************************************/
+
+__asm
+void tn_start_exe(void)
+{
+  EXPORT tn_switch_context_exit
+
+  ldr    r1, =PR_12_15_ADDR        ;-- Load the System 12-15 Priority Register
+  ldr    r0, [r1]
+#if (defined (__ARM_ARCH_6M__ ) && (__ARM_ARCH_6M__  == 1))
+  ldr    r2, =PENDS_VPRIORITY
+  orrs   r0, r0, r2
+#elif ((defined (__ARM_ARCH_7M__ ) && (__ARM_ARCH_7M__  == 1)) || \
+       (defined (__ARM_ARCH_7EM__) && (__ARM_ARCH_7EM__ == 1))     )
+  orr    r0, r0, #PENDS_VPRIORITY
+#endif
+  str    r0, [r1]
+
+;---------------
+
+  ldr    r1, =tn_curr_run_task     ; =tn_next_task_to_run
+  ldr    r2, [r1]
+  ldr    r0, [r2]                  ;-- in r0 - new task SP
+  adds   r0, #32
+  msr    PSP, r0
+
+tn_switch_context_exit
+
+  ldr    r1,  =ICSR_ADDR           ;  Trigger PendSV exception
+  ldr    r0,  =PENDSVSET
+  str    r0,  [r1]
+
+  cpsie  I                         ;  Enable core interrupts
+
+; - Should never reach
+
+  b  .
+
+  ALIGN
+}
+
+__asm
+void tn_disable_irq(void)
+{
+  cpsid  I
+  bx     lr
+}
+
+__asm
+void tn_enable_irq(void)
+{
+  cpsie I
+  bx   lr
+}
+
+__asm
+void tn_switch_context_request(void)
+{
+  ldr    r1,  =ICSR_ADDR
+  ldr    r0,  =PENDSVSET
+  str    r0,  [r1]
+  bx     lr
+
+  ALIGN
+}
+
+#if (defined (__ARM_ARCH_6M__ ) && (__ARM_ARCH_6M__  == 1))
+
+__asm
+int tn_cpu_save_sr(void)
+{
+  mrs    r0, PRIMASK
+  cpsid  I
+  bx     lr
+}
+
+__asm
+void tn_cpu_restore_sr(int sr)
+{
+  msr    PRIMASK, r0
+  bx     lr
+}
+
+#elif ((defined (__ARM_ARCH_7M__ ) && (__ARM_ARCH_7M__  == 1)) || \
+       (defined (__ARM_ARCH_7EM__) && (__ARM_ARCH_7EM__ == 1))     )
+
+__asm
+int tn_cpu_set_basepri(int basepri)
+{
+  mrs    r1, BASEPRI
+  msr    BASEPRI, r0
+  mov    r0, r1
+  bx     lr
+}
+
+__asm
+void tn_cpu_restore_basepri(int basepri)
+{
+  msr    BASEPRI, r0
+  bx     lr
+}
+
+__asm
+int ffs_asm(unsigned int val)
+{
+  mov    r1, r0                    ;-- tmp = in
+  rsbs   r0, r1, #0                ;-- in = -in
+  ands   r0, r0, r1                ;-- in = in & tmp
+  CLZ.W  r0, r0
+  rsb    r0, r0, #0x20             ;-- 32 - in
+  bx     lr
+}
+
+#endif
+
+unsigned int * tn_stack_init(void *task_func, unsigned int *stack_start, void *param)
+{
+  unsigned int *stk = ++stack_start;            //-- Load stack pointer
+
+  *(--stk) = 0x01000000L;                       //-- xPSR
+  *(--stk) = (unsigned int)task_func;           //-- Entry Point (1 for THUMB mode)
+  *(--stk) = (unsigned int)task_exit;           //-- R14 (LR)    (1 for THUMB mode)
+  *(--stk) = 0x12121212L;                       //-- R12
+  *(--stk) = 0x03030303L;                       //-- R3
+  *(--stk) = 0x02020202L;                       //-- R2
+  *(--stk) = 0x01010101L;                       //-- R1
+  *(--stk) = (unsigned int)param;               //-- R0 - task's function argument
+  *(--stk) = 0x11111111L;                       //-- R11
+  *(--stk) = 0x10101010L;                       //-- R10
+  *(--stk) = 0x09090909L;                       //-- R9
+  *(--stk) = 0x08080808L;                       //-- R8
+  *(--stk) = 0x07070707L;                       //-- R7
+  *(--stk) = 0x06060606L;                       //-- R6
+  *(--stk) = 0x05050505L;                       //-- R5
+  *(--stk) = 0x04040404L;                       //-- R4
+
+  return stk;
+}
+
+__asm
+void PendSV_Handler(void)
+{
+  PRESERVE8
+#if (defined (__ARM_ARCH_6M__ ) && (__ARM_ARCH_6M__  == 1))
+  cpsid  I                          ;   Disable core int
+#elif ((defined (__ARM_ARCH_7M__ ) && (__ARM_ARCH_7M__  == 1)) || \
+       (defined (__ARM_ARCH_7EM__) && (__ARM_ARCH_7EM__ == 1))     )
+  ldr    r0, =__cpp(&max_syscall_interrupt_priority)
+  msr    BASEPRI, r0                ;  Start critical section
+#endif
+
+  ldr    r3, =__cpp(&tn_curr_run_task)      ;  in R3 - =tn_curr_run_task
+  ldr    r1, [r3]                   ;  in R1 - tn_curr_run_task
+  ldr    r2, =__cpp(&tn_next_task_to_run)
+  ldr    r2, [r2]                   ;  in R2 - tn_next_task_to_run
+  cmp    r1, r2
+  beq    exit_context_switch
+
+;-------------------------------------
+
+#if (defined (__ARM_ARCH_6M__ ) && (__ARM_ARCH_6M__  == 1))
+  mrs    r0, psp                    ; in PSP - process(task) stack pointer
+  subs   r0, #32                    ; allocate space for r4-r11
+  stmia  r0!, {r4-r7}
+  mov    r4, r8
+  mov    r5, r9
+  mov    r6, r10
+  mov    r7, r11
+  stmia  r0!, {r4-r7}
+  subs   r0, #32                    ; increment r0 again
+
+  str    r0, [r1]                   ;  save own SP in TCB
+  str    r2, [r3]                   ;  in r3 - =tn_curr_run_task
+  ldr    r0, [r2]                   ;  in r0 - new task SP
+
+  adds   r0, #16
+  ldmia  r0!, {r4-r7}
+  mov    r8, r4
+  mov    r9, r5
+  mov    r10, r6
+  mov    r11, r7
+  subs   r0, #32
+  ldmia  r0!, {r4-r7}
+  adds   r0, #16
+  msr    psp, r0
+
+  ldr    r0, =0xFFFFFFFD
+  mov    lr, r0
+#elif ((defined (__ARM_ARCH_7M__ ) && (__ARM_ARCH_7M__  == 1)) || \
+       (defined (__ARM_ARCH_7EM__) && (__ARM_ARCH_7EM__ == 1))     )
+  mrs    r0, psp                    ;  in PSP - process(task) stack pointer
+  stmdb  r0!, {r4-r11}
+
+  str    r0, [r1]                   ;  save own SP in TCB
+  str    r2, [r3]                   ;  in r3 - =tn_curr_run_task
+  ldr    r0, [r2]                   ;  in r0 - new task SP
+
+  ldmia  r0!, {r4-r11}
+  msr    psp, r0
+  orr    lr, lr, #0x04              ;  Force to new process PSP
+#endif
+
+
+;-------------------------------------
+
+exit_context_switch
+
+#if (defined (__ARM_ARCH_6M__ ) && (__ARM_ARCH_6M__  == 1))
+  cpsie  I                          ;  enable core int
+#elif ((defined (__ARM_ARCH_7M__ ) && (__ARM_ARCH_7M__  == 1)) || \
+       (defined (__ARM_ARCH_7EM__) && (__ARM_ARCH_7EM__ == 1))     )
+  mov    r0, #0
+  msr    BASEPRI, r0                ;  End critical section
+#endif
+
+  bx     lr
+
+  ALIGN
+}
+
+/* ----------------------------- End of file ---------------------------------*/
--- src/include/tn_arch.h
+++ src/include/tn_arch.h
@@ -29,30 +29,33 @@
  *
  ******************************************************************************/
 
-#ifndef  _TN_PORT_H_
-#define  _TN_PORT_H_
+#ifndef  _TN_ARCH_H_
+#define  _TN_ARCH_H_
 
 /*******************************************************************************
  *  includes
  ******************************************************************************/
 
-#include "tn_arch_detect.h"
-
-#if defined (__TN_ARCH_ARM7__)
-  #include "../arch/arm/tn_port_arm.h"
-#elif defined (__TN_ARCH_CORTEX_M0__)
-  #include "../arch/cortex_m0/tn_port_cm0.h"
-#elif defined (__TN_ARCH_CORTEX_M3__)
-  #include "../arch/cortex_m3/tn_port_cm3.h"
-#elif defined (__TN_ARCH_CORTEX_M4__)
-  #include "../arch/cortex_m3/tn_port_cm3.h"
-#else
-  #error "Wrong processor architecture"
-#endif
-
 /*******************************************************************************
  *  defines and macros (scope: module-local)
  ******************************************************************************/
+
+#if (defined (__TARGET_ARCH_4T) && (__TARGET_ARCH_4T == 1))
+  #define __ARM_ARCH_4T__           1
+#endif
+
+#if ((defined (__TARGET_ARCH_6_M  ) && (__TARGET_ARCH_6_M   == 1)) || \
+     (defined (__TARGET_ARCH_6S_M ) && (__TARGET_ARCH_6S_M  == 1))   )
+  #define __ARM_ARCH_6M__           1
+#endif
+
+#if (defined (__TARGET_ARCH_7_M ) && (__TARGET_ARCH_7_M  == 1))
+  #define __ARM_ARCH_7M__           1
+#endif
+
+#if (defined (__TARGET_ARCH_7E_M) && (__TARGET_ARCH_7E_M == 1))
+  #define __ARM_ARCH_7EM__          1
+#endif
 
 #define TN_TIMER_STACK_SIZE           64
 #define TN_IDLE_STACK_SIZE            48
@@ -64,6 +67,55 @@
 
 #define TN_NUM_PRIORITY               TN_BITS_IN_INT  //-- 0..31  Priority 0 always is used by timers task
 #define TN_FILL_STACK_VAL             0xFFFFFFFF
+
+#if (defined (__ARM_ARCH_4T__ ) && (__ARM_ARCH_4T__  == 1))
+
+  #define USE_ASM_FFS
+
+  /* - Interrupt processing - processor specific -------------------------------*/
+  #define BEGIN_CRITICAL_SECTION  int tn_save_status_reg = tn_cpu_save_sr();
+  #define END_CRITICAL_SECTION    tn_cpu_restore_sr(tn_save_status_reg);  \
+                                  if (!tn_inside_irq())                   \
+                                    tn_switch_context();
+
+  #define BEGIN_DISABLE_INTERRUPT int tn_save_status_reg = tn_cpu_save_sr();
+  #define END_DISABLE_INTERRUPT   tn_cpu_restore_sr(tn_save_status_reg);
+
+#endif
+
+#if (defined (__ARM_ARCH_6M__ ) && (__ARM_ARCH_6M__  == 1))
+
+  /* - Assembler functions prototypes ----------------------------------------*/
+  #define  tn_switch_context()
+
+  /* - Interrupt processing - processor specific -----------------------------*/
+  #define BEGIN_DISABLE_INTERRUPT int tn_save_status_reg = tn_cpu_save_sr();
+  #define END_DISABLE_INTERRUPT   tn_cpu_restore_sr(tn_save_status_reg);
+
+  #define BEGIN_CRITICAL_SECTION  BEGIN_DISABLE_INTERRUPT
+  #define END_CRITICAL_SECTION    END_DISABLE_INTERRUPT
+
+#endif
+
+#if ((defined (__ARM_ARCH_7M__ ) && (__ARM_ARCH_7M__  == 1)) || \
+     (defined (__ARM_ARCH_7EM__) && (__ARM_ARCH_7EM__ == 1))     )
+
+  #define USE_ASM_FFS
+
+  /* - Assembler functions prototypes ----------------------------------------*/
+  #define  tn_switch_context()
+
+  /* - Interrupt processing - processor specific -----------------------------*/
+  #define BEGIN_DISABLE_INTERRUPT int tn_save_status_reg = tn_cpu_set_basepri(max_syscall_interrupt_priority);
+  #define END_DISABLE_INTERRUPT   tn_cpu_restore_basepri(tn_save_status_reg);
+
+  #define BEGIN_ENABLE_INTERRUPT  int tn_save_status_reg = tn_cpu_set_basepri(0);
+  #define END_ENABLE_INTERRUPT    tn_cpu_restore_basepri(tn_save_status_reg);
+
+  #define BEGIN_CRITICAL_SECTION  BEGIN_DISABLE_INTERRUPT
+  #define END_CRITICAL_SECTION    END_DISABLE_INTERRUPT
+
+#endif
 
 #ifndef BEGIN_CRITICAL_SECTION
   #define BEGIN_CRITICAL_SECTION
@@ -98,19 +150,44 @@ extern unsigned int max_syscall_interrupt_priority;
 extern "C"  {
 #endif
 
+extern void tn_start_exe(void);
+extern unsigned int* tn_stack_init(void *task_func, unsigned int *stack_start, void *param);
 extern void tn_switch_context_request(void);
 extern void tn_switch_context_exit(void);
-extern int tn_cpu_save_sr(void);
-extern void tn_cpu_restore_sr(int sr);
-extern int tn_cpu_set_basepri(int);
-extern void tn_cpu_restore_basepri(int);
-extern void tn_start_exe(void);
-extern int tn_inside_irq(void);
+
+#if (defined (__ARM_ARCH_4T__ ) && (__ARM_ARCH_4T__  == 1))
+
+  extern void tn_switch_context(void);
+  extern void tn_cpu_irq_handler(void);
+  extern void tn_cpu_irq_isr(void);
+  extern void tn_cpu_fiq_isr(void);
+  extern int ffs_asm(unsigned int val);
+  extern int tn_cpu_save_sr(void);
+  extern void tn_cpu_restore_sr(int sr);
+  extern int tn_inside_irq(void);
+
+#endif
+
+#if (defined (__ARM_ARCH_6M__ ) && (__ARM_ARCH_6M__  == 1))
+
+  extern int tn_cpu_save_sr(void);
+  extern void tn_cpu_restore_sr(int sr);
+
+#endif
+
+#if ((defined (__ARM_ARCH_7M__ ) && (__ARM_ARCH_7M__  == 1)) || \
+     (defined (__ARM_ARCH_7EM__) && (__ARM_ARCH_7EM__ == 1))     )
+
+  extern int ffs_asm(unsigned int val);
+  extern int tn_cpu_set_basepri(int);
+  extern void tn_cpu_restore_basepri(int);
+
+#endif
 
 #ifdef __cplusplus
 }  /* extern "C" */
 #endif
 
-#endif  // _TN_PORT_H_
+#endif  // _TN_ARCH_H_
 
 /*------------------------------ End of file ---------------------------------*/
